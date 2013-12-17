c76eddfbf34ac9265345e1594584d1872a06b1a5
bleezmo <joshuarogers10@gmail.com>
2013-12-16 21:12:48 -0500

    reverted back manually to original android project

--- src/com/lehman/android/MainActivity.java
+++ src/com/lehman/android/MainActivity.java
@@ -2,9 +2,7 @@ package com.lehman.android;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.content.Intent;
 import android.view.Menu;
-import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -20,11 +18,6 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
-    }
-    
-    public void toLinearLayout(View view){
-    	Intent intent = new Intent(this,LinearLayoutActivity.class);
-    	startActivity(intent);
     }
     
 }

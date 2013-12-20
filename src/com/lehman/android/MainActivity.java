785872fc21d3a9f4a659208710852f57b3b6244f
bleezmo <joshuarogers10@gmail.com>
2013-12-19 19:10:26 -0500

    finished list lesson

--- src/com/lehman/android/MainActivity.java
+++ src/com/lehman/android/MainActivity.java
@@ -1,30 +1,37 @@
 package com.lehman.android;
 
+import java.util.ArrayList;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Spinner;
 
 public class MainActivity extends Activity {
-
-	private void setUpSpinner(){
-        Spinner spinner = (Spinner) findViewById(R.id.myspinner);
-        // Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-	         R.array.planets_array, android.R.layout.simple_spinner_item);
-        // Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // Apply the adapter to the spinner
-        spinner.setAdapter(adapter);
-	}
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
-        setUpSpinner();
+
+        final ListView listview = (ListView) findViewById(R.id.list);
+        String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+            "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+            "Linux", "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux",
+            "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux", "OS/2",
+            "Android", "iPhone", "WindowsMobile" };
+
+        final ArrayList<String> list = new ArrayList<String>();
+        for (int i = 0; i < values.length; ++i) {
+          list.add(values[i]);
+        }
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+            android.R.layout.simple_list_item_1, list);
+        listview.setAdapter(adapter);
     }
 
     @Override
@@ -34,4 +41,8 @@ public class MainActivity extends Activity {
         return true;
     }
     
+    public void toListEvents(View view){
+    	Intent intent = new Intent(this,ListWithEvents.class);
+    	startActivity(intent);
+    }
 }

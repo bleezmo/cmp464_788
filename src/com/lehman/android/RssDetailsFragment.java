ffffe2883cf2d52245ee2c8c0504306105e2b0ea
bleezmo <joshuarogers10@gmail.com>
2014-02-25 19:27:47 -0500

    fragments (still wip)

--- src/com/lehman/android/RssDetailsFragment.java
+++ src/com/lehman/android/RssDetailsFragment.java
@@ -5,12 +5,27 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class RssDetailsFragment extends Fragment{
-
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.rss_details, container, false);
+		View v = inflater.inflate(R.layout.rss_details, container, false);
+		TextView tv = (TextView) v.findViewById(R.id.rss_details_text);
+		tv.setText(Global.rsslist[currentListItemIndex()].description);
+		return v;
+	}
+
+	public static RssDetailsFragment newInstance(int listItemIndex){
+		RssDetailsFragment fragment = new RssDetailsFragment();
+		Bundle bundle = new Bundle();
+		bundle.putInt("index", listItemIndex);
+		fragment.setArguments(bundle);
+		return fragment;
+	}
+	
+	public int currentListItemIndex(){
+		return getArguments().getInt("index");
 	}
 	
 }
--- src/com/lehman/android/RssListFragment.java
+++ src/com/lehman/android/RssListFragment.java
@@ -1,16 +1,50 @@
 package com.lehman.android;
 
+import com.lehman.android.Global.ListItem;
+
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class RssListFragment extends ListFragment{
-
+	private boolean dualPaneMode;
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		ArrayAdapter<ListItem> adapter = new ArrayAdapter<ListItem>(getActivity(),android.R.layout.simple_list_item_1,Global.rsslist);
+		setListAdapter(adapter);
 		return inflater.inflate(R.layout.rss_list, container, false);
+	}
+
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+        View detailsFrame = getActivity().findViewById(R.id.rss_details_fragment);
+        dualPaneMode = detailsFrame != null && detailsFrame.getVisibility() == View.VISIBLE;
+        if(dualPaneMode){
+        	showDetails(0);
+        }
+	}
+
+	@Override
+	public void onListItemClick(ListView l, View v, int position, long id) {
+		if(dualPaneMode){
+			showDetails(position);
+		}
+	}
+	
+	private void showDetails(int i) {
+		if(dualPaneMode){
+			//first, show that the list item was clicked
+			getListView().setItemChecked(i, true);
+			RssDetailsFragment detailsFragment = (RssDetailsFragment) getFragmentManager().findFragmentById(R.id.rss_details_fragment);
+			if(detailsFragment == null){
+				
+			}
+		}
 	}
 	
 }

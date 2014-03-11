1371e5f223316bda4a44eb1a0dd0f0a2c3584485
bleezmo <joshuarogers10@gmail.com>
2014-03-11 16:37:52 -0400

    finished action bar

--- src/com/lehman/android/SecondActivity.java
+++ src/com/lehman/android/SecondActivity.java
@@ -1,6 +1,8 @@
 package com.lehman.android;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 
@@ -11,6 +13,13 @@ public class SecondActivity extends ActionBarActivity{
 		super.onCreate(savedInstanceState);
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
+	}
+
+	@Override
+	public Intent getSupportParentActivityIntent() {
+		Intent intent = new Intent(this,MainActivity.class);
+		intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_ACTIVITY_SINGLE_TOP);
+		return intent;
 	}
 
 }

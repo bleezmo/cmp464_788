d03d0ecd0211e0992a30776c67b0a60b372ab55b
bleezmo <joshuarogers10@gmail.com>
2014-04-21 12:07:23 -0400

    maps stuff. wip

--- src/com/lehman/android/MainActivity.java
+++ src/com/lehman/android/MainActivity.java
@@ -1,37 +1,54 @@
 package com.lehman.android;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
-
-	private void setUpSpinner(){
-        Spinner spinner = (Spinner) findViewById(R.id.myspinner);
-        // Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-	         R.array.planets_array, android.R.layout.simple_spinner_item);
-        // Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // Apply the adapter to the spinner
-        spinner.setAdapter(adapter);
-	}
-	
+public class MainActivity extends FragmentActivity {
+	GoogleMap mMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
-        setUpSpinner();
+        setUpMapIfNeeded();
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+    private void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+                                .getMap();
+            // Check if we were successful in obtaining the map.
+            if (mMap != null) {
+            	mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+					
+					@Override
+					public void onMapClick(LatLng pt) {
+						Log.i("Lat/Long", pt.latitude+"/"+pt.longitude);
+					}
+				});
+            	Marker marker = mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(40.758546225130104, -73.975471816957))
+                .title("Hello world").draggable(true));
+            	mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+					@Override
+					public boolean onMarkerClick(Marker marker) {
+						Toast.makeText(MainActivity.this, "I've been clicked", Toast.LENGTH_SHORT).show();
+						return false;
+					}
+				});
+            	marker.showInfoWindow();
+            }
+        }
     }
-    
 }
